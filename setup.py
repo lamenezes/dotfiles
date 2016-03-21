55ae495dc6c257383ce8d46161b2238c47d345b5
Luiz Menezes <luiz.menezesf@gmail.com>
2016-03-20 22:01:50 -0300

    add transfer.sh function to .bashrc-extra (old .bashrc-virtualenv)

--- setup.py
+++ setup.py
@@ -3,23 +3,29 @@ import os.path as path
 import subprocess
 
 print('Installing vundle...')
-subprocess.run(['git', 'clone',  'https://github.com/gmarik/vundle.git', path.expanduser('~/.vim/bundle/vundle')])
+subprocess.run(['git', 'clone', 'https://github.com/gmarik/vundle.git',
+                path.expanduser('~/.vim/bundle/vundle')])
 subprocess.run(['vim', '-c', ':BundleInstall'])
 print('Vundle installed')
 
 print('Installing fonts')
 if not path.exists(path.expanduser('~/.fonts')):
     os.mkdir(path.expanduser('~/.fonts'))
-subprocess.run(['cp', 'fonts/Sauce Code Powerline Regular.otf', path.expanduser('~/.fonts')])
+subprocess.run(['cp', 'fonts/Sauce Code Powerline Regular.otf',
+                path.expanduser('~/.fonts')])
 subprocess.run(['fc-cache', '-fv'])
 
-print('Moving dotfiles')
-subprocess.run(['cp', '.bash_aliases', '.gitconfig', '.pythonrc.py', '.vimrc', path.expanduser('~')])
+print('Moving dotfiles...')
+subprocess.run(['cp', '.bash_aliases', '.gitconfig', '.pythonrc.py', '.vimrc',
+               path.expanduser('~')])
 subprocess.run(['cp', '.config/flake8', '.config'])
 
-# appending bashrc-virtualenv to bashrc
-bashrc_venv = subprocess.run(['cat', '.bashrc-virtualenv'], stdout=subprocess.PIPE, universal_newlines=True)
-bashrc = subprocess.run(['cat', path.expanduser('~/.bashrc')], stdout=subprocess.PIPE, universal_newlines=True)
+# appending bashrc-extra to bashrc
+print('Updating .bashrc...')
+bashrc_venv = subprocess.run(['cat', '.bashrc-extra'], stdout=subprocess.PIPE,
+                             universal_newlines=True)
+bashrc = subprocess.run(['cat', path.expanduser('~/.bashrc')], stdout=subprocess.PIPE,
+                        universal_newlines=True)
 bashrc = '{}\n{}'.format(bashrc.stdout, bashrc_venv.stdout)
 with open(path.expanduser('~/.bashrc'), 'w') as f:
     f.write(bashrc)
