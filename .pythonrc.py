5d28e3bca9ffe8599ca0be75ed98266bae5b7294
Luiz Menezes <luiz.menezesf@gmail.com>
2016-08-12 16:38:00 -0300

    simplify .pythonrc

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # vi:si:et:sw=4:ts=4
 
-import datetime
-import os
 import pprint
 import readline
 import rlcompleter
@@ -26,15 +24,3 @@ sys.displayhook = my_displayhook
 
 # enables autocomplete with tab by default
 readline.parse_and_bind('tab: complete')
-
-
-# detects if we`re working on a django project and load all models to M
-if 'DJANGO_SETTINGS_MODULE' in os.environ:
-    from django.db.models.loading import get_models
-
-    class DjangoModels(object):
-        def __init__(self):
-            for m in get_models():
-                setattr(self, m.__name__, m)
-
-    M = DjangoModels()
