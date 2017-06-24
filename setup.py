24bc39ab24f6470ea6b37da419b52040a61d426a
Luiz Menezes <luiz.menzesf@gmail.com>
2017-06-23 21:31:58 -0300

    Fix transfer function to print a newline before command result

--- setup.py
+++ setup.py
@@ -3,29 +3,57 @@ import os.path as path
 import subprocess
 
 print('Installing vundle...')
-subprocess.run(['git', 'clone', 'https://github.com/gmarik/vundle.git',
-                path.expanduser('~/.vim/bundle/vundle')])
-subprocess.run(['vim', '-c', ':BundleInstall'])
+
+subprocess.run([
+    'git', 'clone', 'https://github.com/gmarik/vundle.git',
+    path.expanduser('~/.vim/bundle/vundle'),
+])
+subprocess.run([
+    'vim', '-c', ':BundleInstall'
+])
+
 print('Vundle installed')
 
 print('Installing fonts')
+
 if not path.exists(path.expanduser('~/.fonts')):
     os.mkdir(path.expanduser('~/.fonts'))
-subprocess.run(['cp', 'fonts/Sauce Code Powerline Regular.otf',
-                path.expanduser('~/.fonts')])
-subprocess.run(['fc-cache', '-fv'])
+
+subprocess.run([
+    'cp', 'fonts/Sauce Code Powerline Regular.otf',
+    path.expanduser('~/.fonts'),
+])
+subprocess.run([
+    'fc-cache', '-fv'
+])
 
 print('Moving dotfiles...')
-subprocess.run(['cp', '-ax', '.bash_aliases', '.gitconfig', '.pythonrc.py', '.vimrc',
-                '.tmux.conf', '.virtualenvs', '.bash_profile', path.expanduser('~')])
-subprocess.run(['cp', '-ax', '.config/*', path.expanduser('~/.config')])
+
+subprocess.run([
+    'cp', '-ax',
+    '.bash_aliases', '.gitconfig', '.gitignore_blobal', '.pythonrc.py',
+    '.vimrc', '.tmux.conf', '.virtualenvs', '.bash_profile',
+    path.expanduser('~')
+])
+subprocess.run([
+    'cp', '-ax', '.config/*', path.expanduser('~/.config')
+])
+
 
 # appending bashrc-extra to bashrc
+
+
 print('Updating .bashrc...')
-bashrc_venv = subprocess.run(['cat', '.bashrc-extra'], stdout=subprocess.PIPE,
-                             universal_newlines=True)
-bashrc = subprocess.run(['cat', path.expanduser('~/.bashrc')], stdout=subprocess.PIPE,
-                        universal_newlines=True)
+
+bashrc_venv = subprocess.run(
+    ['cat', '.bashrc-extra'],
+    stdout=subprocess.PIPE, universal_newlines=True
+)
+bashrc = subprocess.run(
+    ['cat', path.expanduser('~/.bashrc')],
+    stdout=subprocess.PIPE, universal_newlines=True
+)
+
 bashrc = '{}\n{}'.format(bashrc.stdout, bashrc_venv.stdout)
 with open(path.expanduser('~/.bashrc'), 'w') as f:
     f.write(bashrc)
